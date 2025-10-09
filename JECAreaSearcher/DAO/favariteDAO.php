ae3b4a8ac2b33acf27489f9d858c32e4feaa3c4c
kino4400 <syo20.nenecha05@gmail.com>
2025-10-09 16:04:17 +0900

    favoriteDAO

---
+++ JECAreaSearcher/DAO/favariteDAO.php
@@ -0,0 +1,30 @@
+<?php 
+
+    require_once './DAO.php';
+
+    class favoriteDAO
+    {
+        public function get_store_id(int $member_id)
+        {
+             $dbh = DAO::get_db_connect();
+
+             //学籍番号をもとにstore_idを取得する
+
+             $sql = "SELECT store_id FROM member_favorite where member_id = :member_id";
+
+             $stmt = $dbh->prepare($sql);
+
+             $stmt->bindvalue(':member_id', $member_id, PDO::PARAM_STR);
+
+             $stmt->execute();
+
+        }
+        
+       
+
+
+    }
+
+
+
+?>
