7f9f087a40733dcedcca691dc31a2dd73aabd51a
kino4400 <syo20.nenecha05@gmail.com>
2025-10-23 14:15:11 +0900

    hashtagDAO.phpの作成、storeDAO.phpにある関数を複数個移籍

--- DAO/storeDAO.php
+++ DAO/storeDAO.php
@@ -16,43 +16,24 @@ class Store
     public int $count;
 }
 
-class Hashtag
-{
-    public int $hashtag_id;
-    public string $hashtag_name;
-}
+
 
 class StoreDAO
 {
-    function get_hashtag_id()
-    {
-        //ハッシュタグID取得
-        $dbh = DAO::get_db_connect();
 
-        $sql = "select sh.hashtag_id from store as s INNER JOIN store_hashtag as sh 
-                on s.store_id = sh.store_id";
-
-        $stmt = $dbh->prepare($sql);
-
-        $stmt->execute();
-        return $stmt->fetch();
-    }
-
-    function get_hashtag_name($hashtag_id)
+    function get_store_id_by_store_address(string $store_address)
     {
         $dbh = DAO::get_db_connect();
-        //ハッシュタグ名取得
-        $hashtag_id = $this->get_hashtag_id();
 
-        $sql = "select h.hashtag_name from hashtag as h INNER JOIN store_hashtag as sh 
-                on h.hashtag_id = sh.hashtag_id
-                where h.hashtag_id = :hashtag_id";
+        $sql = "select store_id from store where store_address = :store_address";
 
         $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':store_address', $store_address, PDO::PARAM_STR);
         $stmt->execute();
-        $stmt->bindValue(':hashtag_id', $hashtag_id, PDO::PARAM_STR);
+
         return $stmt->fetch();
     }
+    
 
     function search_by_keyword(string $keyword)
     {
@@ -82,67 +63,11 @@ class StoreDAO
         return $count;
     }
 
-    function search_by_keyword_to_hashtag_name(string $keyword)
-    {
-        //ハッシュタグ検索
-        $dbh = DAO::get_db_connect();
-        $hashtag_id = $this->get_hashtag_id();
-        $hashtag_name = $this->get_hashtag_name($hashtag_id);
+    
 
-        $sql = " select * from store where :keyword LIKE :hashtag_name";
-
-        $stmt = $dbh->prepare($sql);
-
-        $stmt->bindValue(':keyword', '%' . $keyword . '%', PDO::PARAM_STR);
-        $stmt->bindValue(':hashtag_name', '%' . $hashtag_name . '%', PDO::PARAM_STR);
-        $stmt->execute();
-        return $stmt->fetch();
-    }
-
-    public function hashtag_id_search(string $hashtag_name)
-    {
-        $dbh = DAO::get_db_connect();
-
-        $sql = "SELECT hashtag_id FROM hashtag
-                WHERE hashtag_name = :hashtag_name";
-
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(':hashtag_name', $hashtag_name, PDO::PARAM_STR);
-        $stmt->execute();
-        $hashtag = $stmt->fetchObject('hashtag');
-
-        return $hashtag;
-    }
-
-    public function hashtag_name_insert(string $hashtag_name)
-    {
-        $dbh = DAO::get_db_connect();
-
-        //ハッシュタグテーブルに追加されるハッシュタグ名がないとき
-        if (!$this->hashtag_id_search($hashtag_name)) {
-            $sql = "INSERT INTO hashtag
-                VALUES(':hashtag_name')";
-
-            $stmt = $dbh->prepare($sql);
-            $stmt->bindValue(':hashtag_name', $hashtag_name, PDO::PARAM_STR);
-            $stmt->execute();
-        }
-    }
+   
      //ストアハッシュタグテーブルにstoreidとhashtagidを入れる
-    public function hashtag_insert($store_id,$hashtag_id){
-
-        $dbh = DAO::get_db_connect();
-       
-        $sql = "INSERT INTO store_hashtag
-                VALUES(:store_id,:hashtag_id)";
-        
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(':store_id',$store_id,PDO::PARAM_INT);
-        $stmt->bindValue(':hashtag_id',$hashtag_id,PDO::PARAM_INT);
-        $stmt->execute();
-
-    }
-
+   
     public function image_insert($store_id,$image_name){
         $dbh = DAO::get_db_connect();
 
